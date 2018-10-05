6ee5341d695447326ce7cab2d9f6c76530bcc951
Kenichi Mito <kenichi-mito@cookpad.com>
2018-10-05 10:25:53 +0900

    add a script to check result
    - from https://cookpad.slack.com/archives/C4P1WR3R9/p1538702605000100

---
+++ check.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require "open3"
+
+Dir.glob("test*.rb").sort.each do |rb|
+  ruby_result, _status = Open3.capture2e("ruby #{rb}")
+  interp_result, _status = Open3.capture2e("./interp #{rb}")
+  if ruby_result == interp_result
+    puts ". #{rb}"
+  else
+    puts "x #{rb}"
+    puts "#{interp_result}\n"
+  end
+end
